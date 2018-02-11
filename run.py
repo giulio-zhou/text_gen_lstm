592eb115cbe4185b18901963bb51a8d8297de141
Giulio Zhou <giuliozhou8@gmail.com>
2018-02-11 04:57:49 +0000

    Redo to sample subsequences rather than pad

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from keras.layers import Dense, Embedding, LSTM
+from keras.layers import Dense, Embedding, LSTM, TimeDistributed
 from keras.models import Sequential, load_model
 import h5py
 import numpy as np
@@ -6,81 +6,98 @@ import pandas as pd
 import string
 import sys
 
-# Set to a bit greater than 280 due to rendering of some special characters.
-max_data_length = 350
-sentence_start_token = "S_START"
-sentence_end_token = "S_END"
+SEQ_LENGTH = 50
 
 def process_twitter(path):
     data = pd.read_csv(path, delimiter='|',
             names=['handle', 'timestamp', 'date', 'text'])
     return data.text.tolist()
 
-def process_sentences_as_char(data):
-    num_vocab = 128 + 2 # ASCII characters and SENTENCE_START/END
-    printable = set(string.printable)
-    index_to_word = [chr(i) for i in range(128)] + \
-                    [sentence_start_token, sentence_end_token]
-    word_to_index = {token: i for i, token in enumerate(index_to_word)}
-    filtered_data = map(lambda x: filter(lambda y: y in printable, x), data)
-    return index_to_word, word_to_index, filtered_data
+def map_index_to_char():
+    index_to_char = [chr(i) for i in range(128)]
+    return index_to_char
 
-def create_structured_data(path, dataset_path):
-    data = process_twitter(csv_path)
-    index_to_char, char_to_index, data = process_sentences_as_char(data)
-    hdf5_file = h5py.File(dataset_path, mode='w')
-    hdf5_file.create_dataset(
-        'data', (len(data), max_data_length + 2, 128 + 2), 'u1')
-    start_index = char_to_index[sentence_start_token]
-    end_index = char_to_index[sentence_end_token]
-    for i, string in enumerate(data):
-        data_point = np.zeros((max_data_length + 2, 128 + 2), dtype=np.uint8)
-        data_point[0, start_index] = 1
-        char_indices = map(lambda x: char_to_index[x], string)
-        data_point[np.arange(1, len(string) + 1), char_indices] = 1
-        # Pad the end of the data with more sentence end tokens.
-        data_point[len(string) + 1:, end_index] = 1
-        # Add to data file.
-        hdf5_file['data'][i] = data_point
-    # Test that tweets look reasonable.
-    # for i in range(len(data)):
-    #     data_point = hdf5_file['data'][i]
-    #     vals = [index_to_char[np.argmax(v)] for v in data_point]
-    #     print(''.join(vals))
-    return index_to_char, char_to_index
+def map_char_to_index():
+    index_to_char = map_index_to_char()
+    char_to_index = {token: i for i, token in enumerate(index_to_char)}
+    return char_to_index
+
+def process_sentences_as_char(data):
+    num_vocab = 128 # ASCII characters
+    printable = set(string.printable)
+    filtered_data = map(lambda x: filter(lambda y: y in printable, x), data)
+    return filtered_data
 
 def lstm_model(input_shape):
     model = Sequential()
     model.add(Dense(200, activation='tanh',
                     input_shape=input_shape))
     model.add(LSTM(200, return_sequences=True))
+                   # input_shape=input_shape))
     model.add(LSTM(200, return_sequences=True))
-    model.add(Dense(128 + 2, activation='softmax'))
+    model.add(TimeDistributed(Dense(128, activation='softmax')))
     return model
+
+def preprocess_batch(data_batch):
+    X, Y = [], []
+    char_to_index = map_char_to_index()
+    for data_point in data_batch:
+        x, y = np.zeros((SEQ_LENGTH, 128)), np.zeros((SEQ_LENGTH, 128))
+        start = np.random.randint(0, len(data_point) - SEQ_LENGTH)
+        x_str = data_point[start:start + SEQ_LENGTH]
+        y_str = data_point[start + 1:start + SEQ_LENGTH + 1]
+        x[np.arange(SEQ_LENGTH), map(lambda c: char_to_index[c], x_str)] = 1
+        y[np.arange(SEQ_LENGTH), map(lambda c: char_to_index[c], y_str)] = 1
+        X.append(x)
+        Y.append(y)
+    return np.array(X), np.array(Y)
 
 def train_rnn_model(model, model_path, data, num_iters, batch_size):
     model.compile(loss='categorical_crossentropy', optimizer='adam')
     # Train by getting the model to regress on next character.
     for i in range(num_iters):
         idx = np.random.choice(np.arange(len(data)), batch_size, replace=False)
-        data_batch = data[sorted(idx)]
-        loss = model.train_on_batch(data_batch, data_batch) 
+        data_batch = [data[j] for j in idx]
+        X, Y = preprocess_batch(data_batch)
+        loss = model.train_on_batch(X, Y) 
         if i % 10 == 0:
             print(i, loss)
     model.save(model_path)
 
-def generate_strings(model_path, num_strings):
+# "Greedy" sampling at each step.
+def generate_strings(model_path, num_strings, str_len):
     model = load_model(model_path)
+    index_to_char = map_index_to_char()
+    generated_strings = []
     for i in range(num_strings):
-        start = None
+        vals = [np.zeros(128)]
+        vals[0][np.random.choice(np.arange(128))] = 1
+        for j in range(str_len - 1):
+            input_seq = np.array(vals)
+            input_seq = input_seq.reshape(1, len(vals), 128)
+            probs = model.predict(input_seq)[0]
+            next_idx = np.random.choice(np.arange(128),
+                                        p=probs[-1].flatten())
+            vals.append(np.zeros(128))
+            vals[-1][next_idx] = 1
+            print(len(vals), str_len)
+        decoded_string = map(lambda x: index_to_char[np.argmax(x)], vals)
+        generated_strings.append(''.join(decoded_string))
+        # print(generated_strings[-1])
+    return generated_strings
 
 if __name__ == '__main__':
     csv_path = "realDonaldTrump_tweets.csv"
     dataset_path = "tweet_data.h5"
     model_path = "trained_lstm.h5"
+    data = process_twitter(csv_path)
+    data = process_sentences_as_char(data)
+    data = filter(lambda x: len(x) > SEQ_LENGTH, data)
     if False:
-        create_structured_data(csv_path, dataset_path)
-    data = h5py.File(dataset_path, mode='r')['data']
-    if True:
-        model = lstm_model(data.shape[1:])
-        train_rnn_model(model, data, 500, 32)
+        input_shape = (None, 128)
+        model = lstm_model(input_shape)
+        print(model.predict(preprocess_batch(data[:5])[0]).shape)
+        train_rnn_model(model, model_path, data, 10000, 32)
+    generated_strings = generate_strings(model_path, 5, 300)
+    for sample_str in generated_strings:
+        print(sample_str)
